325bbf2c503c8a370b915e8686c50a0798ffbabb
leejh <leejh@biztechpartners.co.kr>
2019-03-11 18:07:19 +0900

    ch15

--- src/App.js
+++ src/App.js
@@ -14,17 +14,23 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movieData : [
-          {title:"내이름은칸", poster:"https://upload.wikimedia.org/wikipedia/ko/c/c3/%EB%82%B4_%EC%9D%B4%EB%A6%84%EC%9D%80_%EC%B9%B8_%EC%98%81%ED%99%94_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"},
-          {title:"MoonLight", poster:"http://img.hani.co.kr/imgdb/resize/2017/0306/00500005_20170306.JPG"},
-          {title:"괴물", poster:"http://imgmovie.naver.com/mdi/mi/0398/39841_P03_231710.jpg"},
-          {title:"THE IRON LADY", poster:"http://cdn.collider.com/wp-content/uploads/the-iron-lady-movie-poster-01.jpg"},
-          {title:"aquaman", poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHQS0SJzF8IDyKmtoMsEtI9uHAwxV3wN2-76NTMTS-c4uplOLa"}
-        ]
-      })
-    }, 3000)
+    // setTimeout(() => {
+    //   this.setState({
+    //     movieData : [
+    //       {title:"내이름은칸", poster:"https://upload.wikimedia.org/wikipedia/ko/c/c3/%EB%82%B4_%EC%9D%B4%EB%A6%84%EC%9D%80_%EC%B9%B8_%EC%98%81%ED%99%94_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"},
+    //       {title:"MoonLight", poster:"http://img.hani.co.kr/imgdb/resize/2017/0306/00500005_20170306.JPG"},
+    //       {title:"괴물", poster:"http://imgmovie.naver.com/mdi/mi/0398/39841_P03_231710.jpg"},
+    //       {title:"THE IRON LADY", poster:"http://cdn.collider.com/wp-content/uploads/the-iron-lady-movie-poster-01.jpg"},
+    //       {title:"aquaman", poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHQS0SJzF8IDyKmtoMsEtI9uHAwxV3wN2-76NTMTS-c4uplOLa"}
+    //     ]
+    //   })
+    // }, 3000)
+    fetch('https://reqres.in/api/users?page=2')
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   _renderMovie = () => {

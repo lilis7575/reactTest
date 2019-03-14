a33033bf4722a66e3c32adac1771ded9210ccd6f
leejh <leejh@biztechpartners.co.kr>
2019-03-14 14:00:10 +0900

    ch5 Error Boundaries with Higher Order Components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import { createPortal } from 'react-dom';
 
+
+
+const BoundaryHOC = ProtectedComponent => 
+  class Boundary extends Component {
+    state = {
+      hasErr : false
+    }
+    componentDidCatch = () => {
+      this.setState({
+        hasErr:true
+      })
+    }
+    render() {
+      const { hasErr } = this.state;
+      if(hasErr) {
+        return <ErrorCallBack />;
+      } else {
+        return <ProtectedComponent/>;
+      }
+    }
+  }
+
 class ErrorMaker extends Component {
   state = {
     friends: ["111", "222", "333", "444"]
   };
-
+  
   componentDidMount = () => {
     setTimeout(() => {
       this.setState({
@@ -14,20 +36,27 @@ class ErrorMaker extends Component {
       })
     }, 2000)
   }
-
+  
   render() {
     const { friends } = this.state;
     return friends.map(friend => ` ${friend} `);
   }
 }
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
 
 class Portal extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
   }
 }
+const PPortal = BoundaryHOC(Portal);
+
+
+
 
 const Message = () => "just touch";
+const ErrorCallBack = () => "Error !!!";
 
 
 class ReturnType extends Component {
@@ -41,31 +70,30 @@ class ReturnType extends Component {
   }
 }
 
-const ErrorCallBack = () => "Error !!!";
 
 class App extends Component {
   state = {
-    hassError : false
+    hassError: false
   }
 
   componentDidCatch = (error, errinfo) => {
     console.log(` catch : ${error} / info : ${JSON.stringify(errinfo)}`);
     this.setState({
-      hassError : true
+      hassError: true
     });
   }
 
   render() {
-    const {hassError} = this.state;
+    const { hassError } = this.state;
 
     return (
       <Fragment>
         <ReturnType />
-        <Portal></Portal>
-        {hassError ? <ErrorCallBack/> : <ErrorMaker></ErrorMaker>}
+        <PPortal></PPortal>
+        <PErrorMaker />
       </Fragment>
     );
   }
 }
 
-export default App;
+export default BoundaryHOC(App);

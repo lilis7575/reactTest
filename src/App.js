693cab5a2536753f882ea9fe20861448c7bdb867
leejh <leejh@biztechpartners.co.kr>
2019-03-14 10:49:19 +0900

    ch4 Error Boundaries

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import { createPortal } from 'react-dom';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["111", "222", "333", "444"]
+  };
 
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      })
+    }, 2000)
+  }
+
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
 
 class Portal extends Component {
   render() {
-    return createPortal(<Message/>, document.getElementById("touchme"));
+    return createPortal(<Message />, document.getElementById("touchme"));
   }
 }
 
@@ -24,13 +41,28 @@ class ReturnType extends Component {
   }
 }
 
+const ErrorCallBack = () => "Error !!!";
+
 class App extends Component {
+  state = {
+    hassError : false
+  }
+
+  componentDidCatch = (error, errinfo) => {
+    console.log(` catch : ${error} / info : ${JSON.stringify(errinfo)}`);
+    this.setState({
+      hassError : true
+    });
+  }
 
   render() {
+    const {hassError} = this.state;
+
     return (
       <Fragment>
         <ReturnType />
         <Portal></Portal>
+        {hassError ? <ErrorCallBack/> : <ErrorMaker></ErrorMaker>}
       </Fragment>
     );
   }

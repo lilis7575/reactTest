e880e708b6b146049f6e8eab592ab841a8870d45
leejh <leejh@biztechpartners.co.kr>
2019-03-14 14:25:17 +0900

    ch6 this setStatenull

--- src/App.js
+++ src/App.js
@@ -1,99 +1,47 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import { createPortal } from 'react-dom';
 
 
+const MAX_PIZZAS = 20;
 
-const BoundaryHOC = ProtectedComponent => 
-  class Boundary extends Component {
-    state = {
-      hasErr : false
+const eatPizza = (state, props) => {
+  const { pizzas } = state;
+  if(pizzas < MAX_PIZZAS) {
+    return {
+      pizzas : pizzas + 1
     }
-    componentDidCatch = () => {
-      this.setState({
-        hasErr:true
-      })
-    }
-    render() {
-      const { hasErr } = this.state;
-      if(hasErr) {
-        return <ErrorCallBack />;
-      } else {
-        return <ProtectedComponent/>;
-      }
-    }
+  } else {
+    return null;
   }
+}
 
-class ErrorMaker extends Component {
+class Controlled extends Component {
   state = {
-    friends: ["111", "222", "333", "444"]
+    pizzas: 0
   };
-  
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        friends: undefined
-      })
-    }, 2000)
-  }
-  
-  render() {
-    const { friends } = this.state;
-    return friends.map(friend => ` ${friend} `);
-  }
-}
-const PErrorMaker = BoundaryHOC(ErrorMaker);
 
-
-class Portal extends Component {
-  render() {
-    return createPortal(<Message />, document.getElementById("touchme"));
-  }
-}
-const PPortal = BoundaryHOC(Portal);
-
-
-
-
-const Message = () => "just touch";
-const ErrorCallBack = () => "Error !!!";
-
-
-class ReturnType extends Component {
-
-  render() {
+  render (){
+    const { pizzas } = this.state;
     return (
-      <div className="App">
-        hello!222~~!
-      </div>
-    );
+      <button onClick={this._handleClick}>
+        {` I have eaten ${pizzas} ${pizzas === 1 ? "pizza" : "pizzas" } `}
+      </button>
+    )
+  }
+
+  _handleClick = () => {
+    this.setState(eatPizza);
   }
 }
+
 
 
 class App extends Component {
-  state = {
-    hassError: false
-  }
-
-  componentDidCatch = (error, errinfo) => {
-    console.log(` catch : ${error} / info : ${JSON.stringify(errinfo)}`);
-    this.setState({
-      hassError: true
-    });
-  }
-
   render() {
-    const { hassError } = this.state;
-
     return (
-      <Fragment>
-        <ReturnType />
-        <PPortal></PPortal>
-        <PErrorMaker />
-      </Fragment>
+      <Controlled />
     );
   }
 }
 
-export default BoundaryHOC(App);
+export default App;
